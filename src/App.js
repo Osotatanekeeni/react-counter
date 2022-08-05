6fc3e267a45b0192f77f1e922748b66b45867b59
Osotatanekeeni <36969775+Osotatanekeeni@users.noreply.github.com>
2022-08-05 23:25:46 +0200

    Added functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Button from './components/Button';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  function handleIncreaseClick() {
+    setCount(count + 1);
+  }
+
+  function handleDecreaseClick() {
+    setCount(count - 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple React Counter</h1>
+      <h3>Count: {count}</h3>
+      <div className='buttons'>
+        <Button action="Decrease" onClick={handleIncreaseClick}></Button>
+        <Button action="Increase" onClick={handleDecreaseClick}></Button>
+      </div>
     </div>
   );
 }
